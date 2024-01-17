daad49e4d118759237d2195b4234a7c1a9bc9c7e
NachRusten <jpardo180@gmail.com>
2024-01-17 06:16:57 -0500

    estudiando todo hasta object literal y rest parameters

--- estudiar.js
+++ estudiar.js
@@ -1,77 +1,29 @@
 
-//Arrow functioon, for y for of
+var paises = ['Venezuela','Colombia','Austria', 'Alemania'];
 
-var cosas = ['limpion', 'escoba', 'trapero', 'canasta'];
+var prendas = [
+    {tipo: 'Camisa', color: 'Blanco'},
+    {tipo: 'Franela', color: 'Gris'},
+    {tipo: 'Chemise', color: 'Azul'}
+];
 
-let nombrar = (cosa) => {
-    console.log(`la cosa es ${cosa}`);
+var Colombia = {
+    capital: 'Bogota',
+    moneda: 'Pesos',
+    comida: 'Bandeja paisa',
+    musica: 'Vallenato'
 };
 
-for ( var i = 0; i < cosas.length; i++ ) {
-    nombrar(cosas[i]);
-}
+const Austria = {
+    capital: 'Viena',
+    moneda: 'Euros',
+    comida: 'El buñuelo',
+    musica: 'Gaita'
+};
 
-for ( var cosa of cosas) {
-    nombrar(cosa);
-}
+function comida ({comida}) {
+    console.log(comida);
+};
 
+comida(Austria); 
 
-//while
-var ropa = ['pantalon', 'camisa', 'chaqueta', 'franela'];
-
-function seleccionar (prenda){
-    console.log(`${prenda} fue sacada del armario`);
-}
-
-while (ropa.length > 0) {
-    var prenda = ropa.shift();
-    seleccionar(prenda);
-}
-
-// switch
-
-var paises = ['Colombia', 'Venezuela','Salvador','Italia','Austria','Tokio'];
-
-function capitales(pais) {
-    switch (pais) {
-        case 'Colombia':
-            console.log('Bogota');
-            break;
-        case 'Venezuela':
-            console.log('Caracas');
-            break;
-        case 'Salvador':
-            console.log('San Salvador');
-            break;
-        case 'Italia':
-            console.log('Roma');
-            break;
-        case 'Austria':
-            console.log('Viena');
-            break;
-        default:
-            console.log('No es un pais');
-    }
-}
-
-for (var pais of paises) {    
-    capitales(pais);
-}
-
-
-//arrays
-
-var marcas = ['ferrari', 'toyota', 'lamborguini', 'honda', 'Mclaren'];
-
-console.log(marcas.shift());
-marcas.unshift('Ford');
-console.log(marcas);
-marcas.pop();
-console.log(marcas);
-marcas.push('Kia');
-console.log(marcas);
-
-var buscarMarca = marcas.filter(function (marca){
-    return marcas[1];
-})
-console.log(buscarMarca);
