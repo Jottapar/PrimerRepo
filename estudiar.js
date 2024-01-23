fdf1cbff69fd88ecb7ba338df7a4cd894eaf441d
NachRusten <jpardo180@gmail.com>
2024-01-23 00:36:03 -0500

    estudiado hasta reduce()

--- estudiar.js
+++ estudiar.js
@@ -5,29 +5,78 @@ let paisesAdicionales =  ['New Zeland', 'Ireland'];
 const living =`Quiero vivir en ${paises[3]}`;
 
 
+// saber la posicion de un elemento
+
+console.log(paises.indexOf('Colombia'));
 
 
-// iterar con forEach
-let opcionesPaises = paises.forEach( (pais, index, originalArray) => {
-    
-    console.log(`${index+1} opcion es ${pais} --- forEach`);
+// saber si un elemento en la array comple con la  condicion de 5 letras
+
+let paises5 = paises.some((pais) => {
+    return pais.includes('I'); 
 });
 
+console.log(paises5);
+
+//saber si todos los elementos cumplen con una condicion
+
+let paiseString = paises.every((pais) => {
+    return pais.includes('a');
+    });
+
+console.log(paiseString);
+
+// retornar Australia
+let buscarAustralia = paises.find((pais) => {
+    return pais === 'Australia';
+});
+
+console.log(buscarAustralia);
 
 
-//iterar con while
+//Retornar con at
 
-let i = 0;
-
-while (i < paises.length) {
-    i++
-    console.log(`${i} opcion es ${paises[i-1]} --- while`);
-};
+console.log(paises[1]);
+console.log(paises.at(1));
+console.log(paises.at(-1));
 
 
-//iterar con for
 
-for ( let j = 1; j < paises.length; j++) {
-    console.log(`${j} opcion es ${paises[j-1]} --- for`);
-};
+//saber si un elemento esta en un array
 
+
+console.log(paises.includes('Austria'));
+
+let posicionAlemenia = paises.findIndex((pais) => {
+    return pais === 'Alemania';
+})
+console.log(posicionAlemenia);
+
+let years = [5,10,2,25,7];
+
+years.sort((a,b) => {
+    return a - b;
+});
+
+console.log(years);
+
+
+
+let filtrara8 = paises.filter((pais) => {
+    return pais.length < 8;
+})
+
+let viajar = filtrara8.map((destino) => {
+    return `Opcion ${destino}`
+});
+
+console.log(viajar);
+
+// sumar con reduce
+
+
+let sumararray = years.reduce((acumulado,actual,index) => {
+    return acumulado + actual;
+})
+
+console.log(sumararray);
